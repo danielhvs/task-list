0f6f2176cb31e3f36353ea587a9008c3b8d84a76
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-26 17:04:24 -0300

    start/stop/restart server

--- src/task_list/core.clj
+++ src/task_list/core.clj
@@ -3,6 +3,8 @@
     [io.pedestal.http :as http]
     [io.pedestal.http.route :as route]))
 
+(defonce server (atom nil))
+
 (defn respond-hello [request]
   {:status 200 :body "Hello, world!"})
 
@@ -10,14 +12,23 @@
   (route/expand-routes
     #{["/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-  (http/create-server
-    {::http/routes routes
-     ::http/type :jetty
-     ::http/port 8890}))
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8890})
 
-(defn start []
-  (http/start (create-server)))
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                        (assoc service-map
+                          ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
 
 (comment
-  (start))
+  (restart))
