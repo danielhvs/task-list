1f39176b3b257f031298577ac16991818a09badc
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-26 17:01:42 -0300

    respond-hello world pedestal

--- src/task_list/core.clj
+++ src/task_list/core.clj
@@ -1,2 +1,23 @@
-(ns task-list.core)
+(ns task-list.core
+  (:require
+    [io.pedestal.http :as http]
+    [io.pedestal.http.route :as route]))
 
+(defn respond-hello [request]
+  {:status 200 :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+    #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server []
+  (http/create-server
+    {::http/routes routes
+     ::http/type :jetty
+     ::http/port 8890}))
+
+(defn start []
+  (http/start (create-server)))
+
+(comment
+  (start))
